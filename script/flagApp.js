509241ae1bce906efd5bd8b7a73bbbce41554cc9
Anshul Agrawal <anshul.1007@gmail.com>
2014-12-11 09:27:23 +0530

    Done with setup

--- script/flagApp.js
+++ script/flagApp.js
@@ -1,20 +1,31 @@
 var flagApp = angular.module('flagApp', []);
 
-flagApp.controller('GetflagCtrl', function ($log, $scope, flagService) {
-    $scope.flag = '';
-    $scope.colors = [
-        { color: '#ffffff' },
-        { color: '#007a3d' },
-        { color: '#ff9933' },
-        { color: '#00209f' },
-        { color: '#dd0000' },
-        { color: '#ffce00' },
-        { color: '#ffceff' },
-        { color: '#000000' }
-    ];
+flagApp.constant("STATUS", {
+    "TO_BE_STARTED": 0,
+    "IN_PROGRESS": 1,
+    "DONE": 2
+});
 
-    $scope.getFlag = function () {
-        return $scope.flag = flagService.getFlag().then(
+flagApp.controller('GetflagController', function ($log, $scope, flagService, calculationService, STATUS) {
+    $scope.flag = '';
+    $scope.colors = flagService.getColors();
+    $scope.countries = flagService.getCountries();
+
+    $scope.total = 0;
+    $scope.correct = -1;
+    $scope.attempt = 0;
+
+    $scope.getStatus = function () {
+        $scope.status = calculationService.getStatus($scope.correct, $scope.total);
+    }
+
+    $scope.status = calculationService.getStatus($scope.correct, $scope.total);
+
+    $scope.getFlag = function (country) {
+        $scope.total = 0;
+        $scope.correct = -1;
+        $scope.attempt = 0;
+        return $scope.flag = flagService.getFlag(country).then(
             function (data) {
                 $scope.flag = data.svg;
             });
@@ -23,9 +34,9 @@ flagApp.controller('GetflagCtrl', function ($log, $scope, flagService) {
 
 flagApp.factory('flagService', function ($http, $log, $q) {
     return {
-        getFlag: function () {
+        getFlag: function (country) {
             var deferred = $q.defer();
-            $http.get('svg/countries/india/flag.txt')
+            $http.get('svg/countries/' + country + '/flag.txt?tick=' + new Date().getTime())
               .success(function (data) {
                   deferred.resolve({
                       svg: data
@@ -35,6 +46,41 @@ flagApp.factory('flagService', function ($http, $log, $q) {
                   $log.error(msg, code);
               });
             return deferred.promise;
+        },
+        getColors: function () {
+            return [
+                    { color: '#ffffff' },
+                    { color: '#007a3d' },
+                    { color: '#ff9933' },
+                    { color: '#00209f' },
+                    { color: '#dd0000' },
+                    { color: '#ffce00' },
+                    { color: '#ffceff' },
+                    { color: '#000000' }
+            ];
+        },
+        getCountries: function () {
+            return [
+                        { name: 'India', value: 'india' },
+                        { name: 'Sri Lanka', value: 'srilanka' }
+            ];
+        }
+    }
+});
+
+flagApp.factory('calculationService', function (STATUS, $log, $q) {
+    return {
+        getStatus: function (correct, total) {
+            console.log("correct: " + correct + " total:" + total);
+            if (correct < 0) {
+                return STATUS.TO_BE_STARTED;
+            }
+            else if (correct === total) {
+                return STATUS.DONE;
+            }
+            else {
+                return STATUS.IN_PROGRESS;
+            }
         }
     }
 });
@@ -65,10 +111,34 @@ flagApp.directive('svgWrapper', function () {
         link: function (scope, elem, attrs) {
             attrs.$observe('svgData', function (value) {
                 $(elem).empty().append(scope.flag);
-                $("[actualcolor]").on('click', function () {
-                    $(this).attr("fill", $("#pickedColor").css("background-color"))
+
+                scope.total = $("[actualColor]").size();
+
+                $("[actualColor]").on('click', function () {
+
+                    $(this).attr("fill", $("#pickedColor").css("background-color"));
+
+                    var correct = 0;
+                    $.each($("[actualColor]"), function (key, value) {
+                        var actualColor = $(value).attr("actualColor");
+                        var fillColor = $(value).attr("fill");
+                        if (rgb2hex(fillColor) == actualColor) {
+                            correct++;
+                        }
+                    });
+                    scope.correct = correct;
+                    scope.attempt = scope.attempt + 1;
+                    scope.getStatus();
+                    scope.$apply();
                 });
             })
         }
     };
 });
+
+function rgb2hex(rgb) {
+    if (rgb.indexOf("#") > -1)
+        return rgb;
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    return "#" + ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) + ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) + ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2);
+}
